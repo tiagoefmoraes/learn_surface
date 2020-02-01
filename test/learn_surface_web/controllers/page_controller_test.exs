d346c04371cc2a04615a1d96ef9c99cc546f1f2c
Tiago Moraes <tiagoefmoraes@gmail.com>
2020-02-01 03:04:19 -0300

    fix tests

--- test/learn_surface_web/controllers/page_controller_test.exs
+++ test/learn_surface_web/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule LearnSurfaceWeb.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get(conn, "/")
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+    assert html_response(conn, 200) =~ "Surface Bulma"
   end
 end
