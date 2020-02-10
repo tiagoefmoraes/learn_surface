d9209959c67d12b88ff3c3c4b2d35e996515d02c
Tiago Moraes <tiagoefmoraes@gmail.com>
2020-02-10 20:14:37 -0300

    apply class to SampleCode

--- lib/learn_surface_web/views/surface/sample_code.ex
+++ lib/learn_surface_web/views/surface/sample_code.ex
@@ -1,7 +1,8 @@
 defmodule LearnSurfaceWeb.SampleCode do
   use Surface.MacroComponent
 
-  def translate({_, _attributes, [children], _}, caller) do
+  def translate({_, attributes, [children], _}, caller) do
+    props = Map.new(attributes, fn {attr, value, _} -> {String.to_atom(attr), value} end)
     {:safe, content} =
       Phoenix.HTML.html_escape(
         children
@@ -13,7 +14,7 @@ defmodule LearnSurfaceWeb.SampleCode do
 
     translated =
       Surface.Translator.run(
-        "<Sample><div class=\"demo\">#{children}</div>#{code |> Enum.join()}</Sample>",
+        "<Sample class=\"#{Map.get(props, :class, nil)}\"><div class=\"demo\">#{children}</div>#{code |> Enum.join()}</Sample>",
         caller.line,
         caller,
         caller.file
